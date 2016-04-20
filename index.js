283a1ee5872cf81af5589ee8036c0a1644dcba34
조은 <echo@woowahan.com>
2016-04-20 13:21:43 +0900

    모듈 패키지 수정, 정규표현식 수정

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ let program = require('commander'),
 function getFile(file) {
     return new Promise(function(resolve, reject) {
         fs.readFile(file, 'UTF-8', (error, data) => {
-            let reg = /(?:require\('*)(\S*)(?:'\)*)/g;
+            let reg = /(?:require)\(["']\S*(?:["']\)*)/g;
             let fileList = data.match(reg);
             let moduleList = '';
 
             _.eachRight(fileList, (item) => {
-                let reg = /(gulp\S*)(?:')/g;
+                let reg = /gulp+[-a-zA-Z0-9]*/;
                 let res = item.match(reg)[0];
                 moduleList = moduleList + ' ' + res;
                 fileList.pop();
@@ -32,7 +32,7 @@ program
     .action((file) => {
         getFile(file).then(function(moduleList) {
             console.log("download modules" + moduleList);
-            cmd.run('npm install' + moduleList ' --save-dev');
+            cmd.run('npm install' + moduleList + ' --save-dev');
         })
     })
     .parse(process.argv);
